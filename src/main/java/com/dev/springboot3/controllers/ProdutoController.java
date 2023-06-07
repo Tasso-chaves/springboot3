a3f40560acdc1889d864e9a35b7c0f7d50aa2dd8
Tasso-chaves <tassochav@gmail.com>
2023-06-06 23:44:44 -0300

    Endpont atualizarProduto

--- src/main/java/com/dev/springboot3/controllers/ProdutoController.java
+++ src/main/java/com/dev/springboot3/controllers/ProdutoController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,7 +41,7 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.findAll());
     }
 
-    @GetMapping("/produtos/{id}")
+    @GetMapping("/produto/{id}")
     public ResponseEntity<Object> listarProdutoPorId(@PathVariable(value = "id") UUID id){
         Optional<ProdutoModel> produto0 = produtoRepository.findById(id);
         
@@ -49,5 +50,20 @@ public class ProdutoController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(produto0.get());
+    }
+
+    @PutMapping("/produto/{id}")
+    public ResponseEntity<Object> atualizarProduto(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProdutoRecordDto produtoRecordDto){
+
+        Optional<ProdutoModel> produto0 = produtoRepository.findById(id);
+
+        if(produto0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto nao encontrado!");
+        }
+
+        var produtoModel = produto0.get();
+
+        BeanUtils.copyProperties(produtoRecordDto, produtoModel);
+        return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produtoModel));
     }
 }
