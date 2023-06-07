947af413c5d80e5dca732b3dd8ca1d53e8739939
Tasso-chaves <tassochav@gmail.com>
2023-06-06 23:53:17 -0300

    Endpoint apagarProduto

--- src/main/java/com/dev/springboot3/controllers/ProdutoController.java
+++ src/main/java/com/dev/springboot3/controllers/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,5 +66,19 @@ public class ProdutoController {
 
         BeanUtils.copyProperties(produtoRecordDto, produtoModel);
         return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produtoModel));
+    }
+
+    @DeleteMapping("/produto/{id}")
+    public ResponseEntity<Object> apagarProduto(@PathVariable(value = "id") UUID id){
+
+        Optional<ProdutoModel> produto0 = produtoRepository.findById(id);
+
+        if(produto0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto nao encontrado!");
+        }
+
+
+        produtoRepository.delete(produto0.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Produto excluido com sucesso!");
     }
 }
